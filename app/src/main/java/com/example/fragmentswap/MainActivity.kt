1a03e9e57db66f3685ef9cc225a04291c3cad602
Kevin Held <kevinwayneheld@gmail.com>
2022-03-09 19:53:37 -0600

    first commit

---
+++ app/src/main/java/com/example/fragmentswap/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.fragmentswap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.containerFragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+            findViewById<BottomNavigationView>(R.id.bottom_nav)
+                .setupWithNavController(navController)
+    }
+}
